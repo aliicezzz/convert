ff9455e022428fd93531fcaa12046797e58795c7
Alice Ribeiro <aliceribeiro.work@gmail.com>
2025-04-14 22:55:51 -0300

    feat: finalize currency conversion feature

--- scripts.js
+++ scripts.js
@@ -1,22 +1,27 @@
-const USD = 5.83
-const EUR = 6.38
-const GBP = 7.48
+//Cotação de moedas do dia.
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
 
-// Obtendo o valor digitado
-const form = document.querySelector("form");
-const amount = document.getElementById("amount");
-const currency = document.getElementById("currency");
-// Manipulando o input para receber somente números.
+// Obtendo os elementos do formulário.
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const currency =document.getElementById("currency")
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
+const result = document.getElementById("result")
+
+// Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
-  // Expressão regular que detecta qualquer caractere que não seja um dígito (0-9)
-  const hasCharactersRegex = /\D+/g;
-  // Substitui todos os caracteres não numéricos por uma string vazia, permitindo apenas números no input
-  amount.value = amount.value.replace(hasCharactersRegex, "");
-});
+  const hasCharactersRegex = /\D+/g
+  amount.value = amount.value.replace(hasCharactersRegex, "")
+})
 
+// Captando o evento de submit (enviar) do formulário.
 form.onsubmit = (event) => {
-  event.preventDefault();
-  switch (currency.value) {
+  event.preventDefault()
+
+  switch (currency.value){
     case "USD":
       convertCurrency(amount.value, USD, "US$")
       break
@@ -27,6 +32,44 @@ form.onsubmit = (event) => {
       convertCurrency(amount.value, GBP, "£")
       break
   }
-};
+}
 
-function convertCurrency(amount, price, symbol) { }
+// Função para converter a moeda.
+function convertCurrency(amount, price, symbol){
+  try {
+    // Exibindo a cotação da moeda selecionada.
+    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
+
+    // Calcula o total.
+    let total = amount * price
+
+    // Verifica se o resultado não é um número.
+    if (isNaN(total)) {
+      return alert("Por favor, digite o valor corretamente para converter.")
+    }
+
+    // Formatar o valor total.
+    total = formatCurrencyBRL(total).replace("R$", "")
+
+    // Exibe o resultado total.
+    result.textContent = `${total} Reais`
+
+    // Aplica a classe que exibe o footer para mostrar o resultado.
+    footer.classList.add("show-result")
+  } catch {
+    // Remove a classe do footer removendo ele da tela.
+    footer.classList.remove("show-result")
+    
+    console.log(error)
+    alert("Não foi possível converter. Tente novamente mais tarde.")
+  }
+}
+
+// Formata a moeda em Real Brasileiro.
+function formatCurrencyBRL(value) {
+  // Converte para número para utilizar o toLocaleString para formatar no padrão BRL (R$ 00,00).
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })
+}
